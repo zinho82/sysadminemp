8011ccf723ed6d25f0ac80b1197bd187eac3d9a4
Erick Leal <Erick Leal@DESKTOP-OGUSBM6>
2018-10-23 14:17:40 -0300

    agregar items oc sumar total oc inventario agregar productos autorizacion oc

--- src/FinanzasBundle/Controller/OrdenescompraController.php
+++ src/FinanzasBundle/Controller/OrdenescompraController.php
@@ -4,11 +4,9 @@ namespace FinanzasBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\View\TwitterBootstrap3View;
-
 use BackendBundle\Entity\Ordenescompra;
 use BackendBundle\Entity\ItemsOc;
 
@@ -16,73 +14,69 @@ use BackendBundle\Entity\ItemsOc;
  * Ordenescompra controller.
  *
  */
-class OrdenescompraController extends Controller
-{
+class OrdenescompraController extends Controller {
+
     /**
      * Lists all Ordenescompra entities.
      *
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $queryBuilder = $em->getRepository('BackendBundle:Ordenescompra')->createQueryBuilder('e');
-$query = 'update ordenescompra oc
+        $query = 'update ordenescompra oc
 set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where oc.id=i.ordenescompra)';
         $stmt = $em->getConnection()->prepare($query);
-            $stmt->execute();
-             $query = 'update ordenescompra oc set oc.iva=(oc.subtotal*(19/100))';
-        $stmt= $em->getConnection()->prepare($query);
+        $stmt->execute();
+        $query = 'update ordenescompra oc set oc.iva=(oc.subtotal*(19/100))';
+        $stmt = $em->getConnection()->prepare($query);
         $stmt->execute();
         $query = 'update ordenescompra oc set oc.total=(oc.subtotal+oc.iva)';
-        $stmt= $em->getConnection()->prepare($query);
+        $stmt = $em->getConnection()->prepare($query);
         $stmt->execute();
         list($filterForm, $queryBuilder) = $this->filter($queryBuilder, $request);
         list($ordenescompras, $pagerHtml) = $this->paginator($queryBuilder, $request);
-        
+
         $totalOfRecordsString = $this->getTotalOfRecordsString($queryBuilder, $request);
 
         return $this->render('FinanzasBundle:ordenescompra:index.html.twig', array(
-            'ordenescompras' => $ordenescompras,
-            'pagerHtml' => $pagerHtml,
-            'filterForm' => $filterForm->createView(),
-            'totalOfRecordsString' => $totalOfRecordsString,
-
+                    'ordenescompras' => $ordenescompras,
+                    'pagerHtml' => $pagerHtml,
+                    'filterForm' => $filterForm->createView(),
+                    'totalOfRecordsString' => $totalOfRecordsString,
         ));
     }
-      public function indexAutorizacionAction(Request $request)
-    {
+
+    public function indexAutorizacionAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $queryBuilder = $em->getRepository('BackendBundle:Ordenescompra')->createQueryBuilder('e');
-$query = 'update ordenescompra oc
+        $query = 'update ordenescompra oc
 set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where oc.id=i.ordenescompra)';
         $stmt = $em->getConnection()->prepare($query);
-            $stmt->execute();
-             $query = 'update ordenescompra oc set oc.iva=(oc.subtotal*(19/100))';
-        $stmt= $em->getConnection()->prepare($query);
+        $stmt->execute();
+        $query = 'update ordenescompra oc set oc.iva=(oc.subtotal*(19/100))';
+        $stmt = $em->getConnection()->prepare($query);
         $stmt->execute();
         $query = 'update ordenescompra oc set oc.total=(oc.subtotal+oc.iva)';
-        $stmt= $em->getConnection()->prepare($query);
+        $stmt = $em->getConnection()->prepare($query);
         $stmt->execute();
         list($filterForm, $queryBuilder) = $this->filter($queryBuilder, $request);
         list($ordenescompras, $pagerHtml) = $this->paginator($queryBuilder, $request);
-        
+
         $totalOfRecordsString = $this->getTotalOfRecordsString($queryBuilder, $request);
 
         return $this->render('FinanzasBundle:autorizaciones:index_autorizacion.html.twig', array(
-            'ordenescompras' => $ordenescompras,
-            'pagerHtml' => $pagerHtml,
-            'filterForm' => $filterForm->createView(),
-            'totalOfRecordsString' => $totalOfRecordsString,
-
+                    'ordenescompras' => $ordenescompras,
+                    'pagerHtml' => $pagerHtml,
+                    'filterForm' => $filterForm->createView(),
+                    'totalOfRecordsString' => $totalOfRecordsString,
         ));
     }
 
     /**
-    * Create filter form and process filter request.
-    *
-    */
-    protected function filter($queryBuilder, Request $request)
-    {
+     * Create filter form and process filter request.
+     *
+     */
+    protected function filter($queryBuilder, Request $request) {
         $session = $request->getSession();
         $filterForm = $this->createForm('FinanzasBundle\Form\OrdenescompraFilterType');
 
@@ -107,13 +101,13 @@ set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where
             // Get filter from session
             if ($session->has('OrdenescompraControllerFilter')) {
                 $filterData = $session->get('OrdenescompraControllerFilter');
-                
+
                 foreach ($filterData as $key => $filter) { //fix for entityFilterType that is loaded from session
                     if (is_object($filter)) {
                         $filterData[$key] = $queryBuilder->getEntityManager()->merge($filter);
                     }
                 }
-                
+
                 $filterForm = $this->createForm('FinanzasBundle\Form\OrdenescompraFilterType', $filterData);
                 $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($filterForm, $queryBuilder);
             }
@@ -122,33 +116,30 @@ set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where
         return array($filterForm, $queryBuilder);
     }
 
-
     /**
-    * Get results from paginator and get paginator view.
-    *
-    */
-    protected function paginator($queryBuilder, Request $request)
-    {
+     * Get results from paginator and get paginator view.
+     *
+     */
+    protected function paginator($queryBuilder, Request $request) {
         //sorting
-        $sortCol = $queryBuilder->getRootAlias().'.'.$request->get('pcg_sort_col', 'id');
+        $sortCol = $queryBuilder->getRootAlias() . '.' . $request->get('pcg_sort_col', 'id');
         $queryBuilder->orderBy($sortCol, $request->get('pcg_sort_order', 'desc'));
         // Paginator
         $adapter = new DoctrineORMAdapter($queryBuilder);
         $pagerfanta = new Pagerfanta($adapter);
-        $pagerfanta->setMaxPerPage($request->get('pcg_show' , 10));
+        $pagerfanta->setMaxPerPage($request->get('pcg_show', 10));
 
         try {
             $pagerfanta->setCurrentPage($request->get('pcg_page', 1));
         } catch (\Pagerfanta\Exception\OutOfRangeCurrentPageException $ex) {
             $pagerfanta->setCurrentPage(1);
         }
-        
+
         $entities = $pagerfanta->getCurrentPageResults();
 
         // Paginator - route generator
         $me = $this;
-        $routeGenerator = function($page) use ($me, $request)
-        {
+        $routeGenerator = function($page) use ($me, $request) {
             $requestParams = $request->query->all();
             $requestParams['pcg_page'] = $page;
             return $me->generateUrl('ordenescompra', $requestParams);
@@ -164,12 +155,11 @@ set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where
 
         return array($entities, $pagerHtml);
     }
-    
-    
-    
+
     /*
      * Calculates the total of records string
      */
+
     protected function getTotalOfRecordsString($queryBuilder, $request) {
         $totalOfRecords = $queryBuilder->select('COUNT(e.id)')->getQuery()->getSingleScalarResult();
         $show = $request->get('pcg_show', 10);
@@ -183,61 +173,57 @@ set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where
         }
         return "Showing $startRecord - $endRecord of $totalOfRecords Records.";
     }
-    
-    
 
     /**
      * Displays a form to create a new Ordenescompra entity.
      *
      */
-    public function newAction(Request $request)
-    {
-    
-        $ordenescompra = new Ordenescompra();
-        $form   = $this->createForm('FinanzasBundle\Form\OrdenescompraType', $ordenescompra);
-        $form->handleRequest($request);
+    public function newAction(Request $request) {
 
+        $ordenescompra = new Ordenescompra();
+        $form = $this->createForm('FinanzasBundle\Form\OrdenescompraType', $ordenescompra);
+        $form->handleRequest($request);
+        $numoc = date('YmdGis');
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
+            $estado = $em->getRepository("BackendBundle:Config")->find(57);
             $ordenescompra->setSolicitadoPor($this->getUser());
+            $ordenescompra->setEstado($estado);
+            $ordenescompra->setFechaIngreso(new \DateTime);
+            $ordenescompra->setNumeroOc($numoc);
             $em->persist($ordenescompra);
-            
+
             $em->flush();
-            
+
             $editLink = $this->generateUrl('ordenescompra_edit', array('id' => $ordenescompra->getId()));
-            $this->get('session')->getFlashBag()->add('success', "<a href='$editLink'>New ordenescompra was created successfully.</a>" );
-            
-            $nextAction=  $request->get('submit') == 'save' ? 'ordenescompra' : 'ordenescompra_new';
+            $this->get('session')->getFlashBag()->add('success', "<a href='$editLink'>New ordenescompra was created successfully.</a>");
+
+            $nextAction = $request->get('submit') == 'save' ? 'ordenescompra' : 'ordenescompra_new';
             return $this->redirectToRoute($nextAction);
         }
         return $this->render('FinanzasBundle:ordenescompra:new.html.twig', array(
-            'ordenescompra' => $ordenescompra,
-            'form'   => $form->createView(),
+                    'ordenescompra' => $ordenescompra,
+                    'form' => $form->createView(),
         ));
     }
-    
 
     /**
      * Finds and displays a Ordenescompra entity.
      *
      */
-    public function showAction(Ordenescompra $ordenescompra)
-    {
+    public function showAction(Ordenescompra $ordenescompra) {
         $deleteForm = $this->createDeleteForm($ordenescompra);
         return $this->render('FinanzasBundle:ordenescompra:show.html.twig', array(
-            'ordenescompra' => $ordenescompra,
-            'delete_form' => $deleteForm->createView(),
+                    'ordenescompra' => $ordenescompra,
+                    'delete_form' => $deleteForm->createView(),
         ));
     }
-    
-    
 
     /**
      * Displays a form to edit an existing Ordenescompra entity.
      *
      */
-    public function editAction(Request $request, Ordenescompra $ordenescompra)
-    {
+    public function editAction(Request $request, Ordenescompra $ordenescompra) {
         $deleteForm = $this->createDeleteForm($ordenescompra);
         $editForm = $this->createForm('FinanzasBundle\Form\OrdenescompraType', $ordenescompra);
         $editForm->handleRequest($request);
@@ -246,26 +232,23 @@ set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where
             $em = $this->getDoctrine()->getManager();
             $em->persist($ordenescompra);
             $em->flush();
-            
+
             $this->get('session')->getFlashBag()->add('success', 'Edited Successfully!');
             return $this->redirectToRoute('ordenescompra_edit', array('id' => $ordenescompra->getId()));
         }
         return $this->render('FinanzasBundle:ordenescompra:edit.html.twig', array(
-            'ordenescompra' => $ordenescompra,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
+                    'ordenescompra' => $ordenescompra,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
         ));
     }
-    
-    
 
     /**
      * Deletes a Ordenescompra entity.
      *
      */
-    public function deleteAction(Request $request, Ordenescompra $ordenescompra)
-    {
-    
+    public function deleteAction(Request $request, Ordenescompra $ordenescompra) {
+
         $form = $this->createDeleteForm($ordenescompra);
         $form->handleRequest($request);
 
@@ -277,10 +260,10 @@ set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where
         } else {
             $this->get('session')->getFlashBag()->add('error', 'Problem with deletion of the Ordenescompra');
         }
-        
+
         return $this->redirectToRoute('ordenescompra');
     }
-    
+
     /**
      * Creates a form to delete a Ordenescompra entity.
      *
@@ -288,22 +271,21 @@ set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where
      *
      * @return \Symfony\Component\Form\Form The form
      */
-    private function createDeleteForm(Ordenescompra $ordenescompra)
-    {
+    private function createDeleteForm(Ordenescompra $ordenescompra) {
         return $this->createFormBuilder()
-            ->setAction($this->generateUrl('ordenescompra_delete', array('id' => $ordenescompra->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
+                        ->setAction($this->generateUrl('ordenescompra_delete', array('id' => $ordenescompra->getId())))
+                        ->setMethod('DELETE')
+                        ->getForm()
         ;
     }
-    
+
     /**
      * Delete Ordenescompra by id
      *
      */
-    public function deleteByIdAction(Ordenescompra $ordenescompra){
+    public function deleteByIdAction(Ordenescompra $ordenescompra) {
         $em = $this->getDoctrine()->getManager();
-        
+
         try {
             $em->remove($ordenescompra);
             $em->flush();
@@ -313,15 +295,12 @@ set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where
         }
 
         return $this->redirect($this->generateUrl('ordenescompra'));
-
     }
-    
 
     /**
-    * Bulk Action
-    */
-    public function bulkAction(Request $request)
-    {
+     * Bulk Action
+     */
+    public function bulkAction(Request $request) {
         $ids = $request->get("ids", array());
         $action = $request->get("bulk_action", "delete");
 
@@ -337,7 +316,6 @@ set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where
                 }
 
                 $this->get('session')->getFlashBag()->add('success', 'ordenescompras was deleted successfully!');
-
             } catch (Exception $ex) {
                 $this->get('session')->getFlashBag()->add('error', 'Problem with deletion of the ordenescompras ');
             }
@@ -345,6 +323,5 @@ set oc.subtotal=(select sum(i.valor * i.cantidad) as total from items_oc i where
 
         return $this->redirect($this->generateUrl('ordenescompra'));
     }
-    
 
 }
--- src/FinanzasBundle/Form/OrdenescompraType.php
+++ src/FinanzasBundle/Form/OrdenescompraType.php
@@ -6,66 +6,90 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
-class OrdenescompraType extends AbstractType
-{
+class OrdenescompraType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('numeroOc')
-            ->add('fechaEstimadaCompra')
-            ->add('fechaIngreso')
-            ->add('proveedoresClientes', EntityType::class, array(
-                'class' => 'BackendBundle\Entity\ProveedoresClientes',
-                'choice_label' => 'nombre',
-                'placeholder' => 'Please choose',
-                'empty_data' => null,
-                'required' => false
- 
-            )) 
-            ->add('tipoOc', EntityType::class, array(
-                'class' => 'BackendBundle\Entity\Config',
-                'choice_label' => 'titulo',
-                'placeholder' => 'Please choose',
-                'empty_data' => null,
-                'required' => false
- 
-            )) 
-            ->add('empresa', EntityType::class, array(
-                'class' => 'BackendBundle\Entity\Empresa',
-                'choice_label' => 'nombre',
-                'placeholder' => 'Please choose',
-                'empty_data' => null,
-                'required' => false
- 
-            )) 
-                 ->add('estado', EntityType::class, array(
-                'class' => 'BackendBundle\Entity\Config',
-                'choice_label' => 'nombre',
-                'placeholder' => 'Please choose',
-                'empty_data' => null,
-                'required' => true,
+//                ->add('numeroOc', TextType::class, array(
+//                    'attr' => array(
+//                        'class' => 'form-control'
+//                    )
+//                ))
+                ->add('fechaEstimadaCompra', DateType::class,array(
+                    'attr'  => array(
+                    'class' =>  'form-control',
+                        
+                )
+                ))
+                ->add('proveedoresClientes', EntityType::class, array(
+                    'class' => 'BackendBundle\Entity\ProveedoresClientes',
+                    'choice_label' => 'nombre',
+                    'placeholder' => 'Please choose',
+                    'empty_data' => null,
+                    'required' => true,
+                    
+                    'attr' => array(
+                        'class' => 'form-control'
+                    )
+                ))
+                ->add('tipoOc', EntityType::class, array(
+                    'class' => 'BackendBundle\Entity\Config',
+                    'choice_label' => 'titulo',
+                    'placeholder' => 'Please choose',
+                    'empty_data' => null,
+                    'required' => true,
+                    'attr' => array(
+                        'class' => 'form-control'
+                    ),
                      'query_builder' => function(\Doctrine\ORM\EntityRepository $co) {
                         return $co->createQueryBuilder('u')
-                                ->where('u.pertenece=37')
+                                ->where('u.pertenece=31')
                                 ->orderBy('u.titulo', 'asc');
                     },
- 
-            )) 
+                ))
+                ->add('empresa', EntityType::class, array(
+                    'class' => 'BackendBundle\Entity\Empresa',
+                    'choice_label' => 'nombre',
+                    'placeholder' => 'Please choose',
+                    'empty_data' => null,
+                    'required' => true,
+                    'attr' => array(
+                        'class' => 'form-control'
+                    )
+                ))
+//                ->add('estado', EntityType::class, array(
+//                    'class' => 'BackendBundle\Entity\Config',
+//                    'choice_label' => 'titulo',
+//                    'placeholder' => 'Please choose',
+//                    'empty_data' => null,
+//                    'required' => true,
+//                    'query_builder' => function(\Doctrine\ORM\EntityRepository $co) {
+//                        return $co->createQueryBuilder('u')
+//                                ->where('u.pertenece=37')
+//                                ->orderBy('u.titulo', 'asc');
+//                    },
+//                    'attr' => array(
+//                        'class' => 'form-control'
+//                    )
+//                ))
         ;
     }
-    
+
     /**
      * @param OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'BackendBundle\Entity\Ordenescompra'
         ));
     }
+
 }
