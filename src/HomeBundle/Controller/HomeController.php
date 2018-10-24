f18de1c38bc25c6fb8dc2f5cae7823ea57583612
Erick Leal <Erick Leal@DESKTOP-OGUSBM6>
2018-10-24 08:35:51 -0300

    notificacion OC

--- src/HomeBundle/Controller/HomeController.php
+++ src/HomeBundle/Controller/HomeController.php
@@ -11,10 +11,11 @@ class HomeController extends Controller {
 
     public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
-        
+        $Notificaciones=$em->getRepository("BackendBundle:Notificaciones")->findAll();
         return $this->render('HomeBundle:home:index.html.twig', array(
                     'notiFinanzas' => null,
                     'facturas' => null,
+            'notifica'  => $Notificaciones,
             'pantallas' =>  null,
         ));
     }
