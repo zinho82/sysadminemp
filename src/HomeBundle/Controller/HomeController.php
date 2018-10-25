c8396e01cff772b0ed3ff0f3c64e8cc792b79321
Erick Leal <Erick Leal@DESKTOP-OGUSBM6>
2018-10-25 15:25:21 -0300

    home:  facturas pendientes

--- src/HomeBundle/Controller/HomeController.php
+++ src/HomeBundle/Controller/HomeController.php
@@ -11,12 +11,18 @@ class HomeController extends Controller {
 
     public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
-        $Notificaciones=$em->getRepository("BackendBundle:Notificaciones")->findAll();
+        $Notificaciones = $em->getRepository("BackendBundle:Notificaciones")->findAll();
+        $repo_factura = $em->getRepository("BackendBundle:Facturas");
+        $Facturas = $repo_factura->createQueryBuilder('c')
+                        ->where("c.estadoPago != 27")
+                        ->orderBy('c.fechaIngreso', 'desc')
+                        ->getQuery()->getResult();
         return $this->render('HomeBundle:home:index.html.twig', array(
                     'notiFinanzas' => null,
                     'facturas' => null,
-            'notifica'  => $Notificaciones,
-            'pantallas' =>  null,
+                    'notifica' => $Notificaciones,
+                    'facturas' => $Facturas,
+                    'pantallas' => null,
         ));
     }
 
