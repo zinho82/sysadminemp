b219cd1a549d999a8c7e63f73ac1bcbc2f7b3bdb
Erick Leal <Erick Leal@DESKTOP-OGUSBM6>
2018-10-26 12:06:12 -0300

    banco 	crud facturas 	add campo tipo gasto oc 	add campo proyecto campaña add nivel 3 menu config: 	crud general

--- src/ConfigBundle/Form/ConfigType.php
+++ src/ConfigBundle/Form/ConfigType.php
@@ -5,6 +5,9 @@ namespace ConfigBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class ConfigType extends AbstractType
 {
@@ -15,8 +18,27 @@ class ConfigType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titulo')
-            ->add('pertenece')
+            ->add('titulo', TextType::class,array(
+                 'attr' => array(
+                        'class' => 'form-control',
+                    ),
+            ))
+            ->add('pertenece', EntityType::class, array(
+                'class' => 'BackendBundle\Entity\Config',
+                'choice_label' => 'titulo',
+                'placeholder' => 'Please choose',
+                'empty_data' => null,
+                'required' => false,
+                'query_builder' => function(\Doctrine\ORM\EntityRepository $co) {
+                        return $co->createQueryBuilder('u')
+                                ->where('u.pertenece is null')
+                                ->orderBy('u.titulo', 'asc');
+                    },
+                             'attr' => array(
+                        'class' => 'form-control',
+                    ),
+ 
+            )) 
         ;
     }
     
--- web/app_dev.php
+++ web/app_dev.php
@@ -11,7 +11,7 @@ use Symfony\Component\Debug\Debug;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR']) 
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])  
     || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '10.15.8.83','190.46.111.163','::1']) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden'); 
