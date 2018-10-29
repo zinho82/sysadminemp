b5d183d281015a6c4eb8e312838b928aa81fc034
Erick Leal <Erick Leal@DESKTOP-OGUSBM6>
2018-10-29 14:45:13 -0300

    Campana. 	crud 	asignacion de fechas

--- src/EmpresaBundle/Form/CampanaType.php
+++ src/EmpresaBundle/Form/CampanaType.php
@@ -6,6 +6,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
 
 class CampanaType extends AbstractType
 {
@@ -16,19 +19,55 @@ class CampanaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('descripcion')
-            ->add('nombre')
-            ->add('fechaFacturacion')
-            ->add('fechaInicio')
-            ->add('fechaTermino')
-            ->add('proveedoresClientes', EntityType::class, array(
+                 ->add('nombre', TextType::class,array(
+                 'attr'  =>  array(
+                    'class' =>  'form-control',
+                ),
+                     'label'    =>'Nombre Campaña / Proyecto'
+            ))
+            ->add('descripcion', CKEditorType::class,array(
+                'attr'  =>  array(
+                    'class' =>  'form-control',
+                   
+                ),
+              
+            ))
+                 ->add('proveedoresClientes', EntityType::class, array(
                 'class' => 'BackendBundle\Entity\ProveedoresClientes',
                 'choice_label' => 'nombre',
                 'placeholder' => 'Please choose',
                 'empty_data' => null,
-                'required' => false
+                'required' => false,
+                 'attr'  =>  array(
+                    'class' =>  'form-control',
+                ),
+                'label' =>  'Proveedor',
+                      'query_builder' => function(\Doctrine\ORM\EntityRepository $co) {
+                        return $co->createQueryBuilder('u')
+                                ->where('u.proveedorCliente=36')
+                                ->orderBy('u.nombre', 'asc');
+                    },
  
             )) 
+           
+            ->add('fechaFacturacion', DateType::class,array(
+                 'attr'  =>  array(
+                    'class' =>  'form-control',
+                ),
+                  'data'  =>  new \DateTime,
+            ))
+            ->add('fechaInicio', DateType::class,array(
+                 'attr'  =>  array(
+                    'class' =>  'form-control',
+                ),
+                 'data'  =>  new \DateTime,
+            ))
+            ->add('fechaTermino',DateType::class,array(
+                 'attr'  =>  array(
+                    'class' =>  'form-control',
+                ),
+            ))
+           
         ;
     }
     
